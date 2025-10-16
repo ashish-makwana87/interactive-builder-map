2d36364dcf8a172719d75c9883a398117b23e856
ashish-makwana87 <ashishn.makwana@gmail.com>
2025-10-16 11:17:42 +0530

    setup

---
+++ app/map/page.tsx
@@ -0,0 +1,16 @@
+import MapImg from "@/public/Verona.jpg";
+import Image from "next/image";
+
+function BuilderMap() {
+
+  return (
+    <section className='alignment py-10 md:py-20'>
+     <div className="mb-10 border">
+     <h1>map image</h1>
+     </div>
+     <a href="/" className="link">Home</a>
+    </section>
+  )
+}
+
+export default BuilderMap;
